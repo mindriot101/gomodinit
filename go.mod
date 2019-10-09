f631320105c1b8c3a5e38d5f6ebbde431aa9f846
Simon Walker <s.r.walker101@googlemail.com>
2019-10-09 16:58:11 +0100

    Create tool for creating modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mindriot101/gomodinit
+
+go 1.12
+
+require github.com/spf13/cobra v0.0.5
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	username string
+	hostname string
+)
+
+func calculateModStub() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	return path.Base(dir), nil
+}
+
+func gomodinit(cmd *cobra.Command, args []string) {
+	modStub, err := calculateModStub()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	modName := fmt.Sprintf("%s/%s/%s", hostname, username, modStub)
+	log.Printf("Creating module under %s", modName)
+
+	var out bytes.Buffer
+	goCmd := exec.Command("go", "mod", "init", modName)
+	goCmd.Stderr = &out
+
+	if err = goCmd.Run(); err != nil {
+		log.Fatalf("Error %v: %s\n", err, out.String())
+	}
+}
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "gomodinit",
+		Short: "",
+		Long:  "",
+		Run:   gomodinit,
+	}
+
+	rootCmd.PersistentFlags().StringVarP(&username, "user", "u", "mindriot101", "username to assign package to")
+	rootCmd.PersistentFlags().StringVarP(&hostname, "host", "H", "github.com", "host to assign package to")
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
