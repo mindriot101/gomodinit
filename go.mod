04e9c42cb2d03b7e31234921b0498648534739ff
Simon Walker <s.r.walker101@googlemail.com>
2020-05-22 23:23:59 +0100

    Use zap for logging

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mindriot101/gomodinit
 
 go 1.12
 
-require github.com/spf13/cobra v0.0.5
+require (
+	github.com/spf13/cobra v0.0.5
+	go.uber.org/zap v1.15.0
+)
--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 var (
 	username string
 	hostname string
+	verbose  bool
 )
 
 func calculateModStub() (string, error) {
@@ -26,20 +28,35 @@ func calculateModStub() (string, error) {
 }
 
 func gomodinit(cmd *cobra.Command, args []string) {
+	// Set up logging
+	config := zap.NewDevelopmentConfig()
+	if verbose {
+		config.Level.SetLevel(zap.InfoLevel)
+	} else {
+		config.Level.SetLevel(zap.WarnLevel)
+	}
+	logger, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
+
 	modStub, err := calculateModStub()
 	if err != nil {
-		log.Fatal(err)
+		sugar.Fatal(err)
 	}
 
 	modName := fmt.Sprintf("%s/%s/%s", hostname, username, modStub)
-	log.Printf("Creating module under %s", modName)
+	sugar.Infof("Creating module under %s", modName)
 
 	var out bytes.Buffer
 	goCmd := exec.Command("go", "mod", "init", modName)
 	goCmd.Stderr = &out
 
 	if err = goCmd.Run(); err != nil {
-		log.Fatalf("Error %v: %s\n", err, out.String())
+		sugar.Fatalf("Error %v: %s\n", err, out.String())
 	}
 }
 
@@ -53,6 +70,7 @@ func main() {
 
 	rootCmd.PersistentFlags().StringVarP(&username, "user", "u", "mindriot101", "username to assign package to")
 	rootCmd.PersistentFlags().StringVarP(&hostname, "host", "H", "github.com", "host to assign package to")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose logging")
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
